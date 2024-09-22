378e72733cbf616c3e931adc18464a0a5ac95cae
dabhirutam <dabhirutam34@gmail.com>
2024-09-22 16:11:17 +0530

    BHAGAVADGITA-API

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,55 @@
-let chs = document.getElementById('chs');
+let chapter = document.getElementById('chapter');
+let slokNo = document.getElementById('slokNo');
+let slok = document.getElementById('slok');
+let chName = document.getElementById('chName');
 
-fetch('https://vedicscriptures.github.io/chapters/').then(res => res.json()).then((rec) => {
-    console.log("Rec",rec);
-    
-    rec.forEach(data => {
-        chs.innerHTML += `<div class="col-md-6">
-                                <a href="chapter.html" class="p-4 shadow rounded d-block text-decoration-none text-dark" onclick="return chapterView(${data.chapter_number})">
-                                    <p class="fw-bold fs-5">अध्याय - ${data.chapter_number}</p>
-                                    <h2>${data.name}</h2>
-                                    <p>${data.summary.hi.slice(0, 270)} . . .</p>
-                                    <span><i class="bi bi-list-ul"></i> ${data.verses_count} Verses</span>
-                                </a>
-                            </div>`
+async function chapters() {
+    let response = await fetch('https://vedicscriptures.github.io/chapters/');
+    let rec = await response.json();
+
+    rec.forEach((data, index) => {
+        chapter.innerHTML += `<li onclick="return viewSlNo(${data.chapter_number},${data.verses_count})" class="chs text-start ps-4 p-2 mb-3 rounded-3 shadow bg-dark border-5 border-bottom border-black ${data.chapter_number == 1 ? 'activeCh' : ''}">${index+1}. &nbsp;${data.name}</li>`;
     });
 
-}).catch(error => console.log("Error", error));
+};
 
-const chapterView = no => localStorage.setItem("chapterNo", JSON.stringify(no));
+const activeBtn = (cl, act) => {
+    let btn = document.querySelectorAll(cl);
+
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener("click", function () {
+            let current = document.getElementsByClassName(act);
+            current[0].className = current[0].className.replace(act, "");
+            this.className += act;
+        });
+    };
+};
+
+const viewSlNo = (no, count) => {
+    viewSlok(no, 1), activeBtn('.chs', 'activeCh');
+
+    chName.innerHTML = `अध्याय - ${no}`;
+    slokNo.innerHTML = '';
+    for (let r = 1; r <= count; r++) {
+        slokNo.innerHTML += `<li onclick="return viewSlok(${no},${r})" class="sloks p-2 mb-3 rounded-3 shadow bg-dark border-5 border-bottom border-black  ${r == 1 ? 'activeSl' : ''}">श्लोक - ${r}</li>`;
+    };
+};
+
+async function viewSlok(chNo, slNo) {
+    activeBtn('.sloks', 'activeSl');
+
+    let response = await fetch(`https://vedicscriptures.github.io/slok/${chNo}/${slNo}/`);
+    let rec = await response.json();
+
+    slok.innerHTML = `<div class="fs-5 fw-medium d-flex flex-column row-gap-4">
+                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">${rec._id.replace('BG', 'BG - ')}</h2>
+                        <p class="fs-2 text-black" style="text-shadow: 0px 0px 15px #ff6f08">${rec.slok.replaceAll('\n', '<br>')}</p>
+                        <p>${rec.transliteration.replaceAll('\n', '<br>')}</p>
+                        <hr style="border: 1px solid #000;">
+                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">Translation</h2>
+                        <p class="text-start mb-5">${rec.siva.et}</p>
+                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">Commentary</h2>
+                        <p class="text-start">${rec.siva.ec}</p>
+                    </div>`;
+};
+chapters(), viewSlNo(1, 47), viewSlok(1, 1);
