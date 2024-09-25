53ca5c092551a48099705f8eecdd82f6907f6adf
dabhirutam <dabhirutam34@gmail.com>
2024-09-25 18:20:31 +0530

    Add-Responsive-Layout

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
-let chapter = document.getElementById('chapter');
-let slokNo = document.getElementById('slokNo');
+let chapter = document.querySelectorAll('.chapter');
+let slokNo = document.querySelectorAll('.slokNo');
 let slok = document.getElementById('slok');
 let chName = document.getElementById('chName');
 
@@ -8,7 +8,8 @@ async function chapters() {
     let rec = await response.json();
 
     rec.forEach((data, index) => {
-        chapter.innerHTML += `<li onclick="return viewSlNo(${data.chapter_number},${data.verses_count})" class="chs text-start ps-4 p-2 mb-3 rounded-3 shadow bg-dark border-5 border-bottom border-black ${data.chapter_number == 1 ? 'activeCh' : ''}">${index+1}. &nbsp;${data.name}</li>`;
+        chapter[1].innerHTML += `<li onclick="return viewSlNo(${data.chapter_number},${data.verses_count})" class="chs-1 text-start ps-3 p-2 mb-3 rounded-3 shadow bg-dark border-5 border-bottom border-black ${data.chapter_number == 1 ? 'activeCh-1' : ''}">${index+1}. &nbsp;${data.name}</li>`;
+        chapter[0].innerHTML += `<li onclick="return viewSlNo(${data.chapter_number},${data.verses_count})" class="chs-0 text-start ps-3 p-2 mb-3 rounded-3 shadow bg-dark border-5 border-bottom border-black ${data.chapter_number == 1 ? 'activeCh-0' : ''}">${index+1}. &nbsp;${data.name}</li>`;
     });
 
 };
@@ -26,30 +27,30 @@ const activeBtn = (cl, act) => {
 };
 
 const viewSlNo = (no, count) => {
-    viewSlok(no, 1), activeBtn('.chs', 'activeCh');
+    viewSlok(no, 1), activeBtn('.chs-1', 'activeCh-1'), activeBtn('.chs-0', 'activeCh-0');
 
     chName.innerHTML = `अध्याय - ${no}`;
-    slokNo.innerHTML = '';
+    slokNo[0].innerHTML = slokNo[1].innerHTML = '';
     for (let r = 1; r <= count; r++) {
-        slokNo.innerHTML += `<li onclick="return viewSlok(${no},${r})" class="sloks p-2 mb-3 rounded-3 shadow bg-dark border-5 border-bottom border-black  ${r == 1 ? 'activeSl' : ''}">श्लोक - ${r}</li>`;
+        slokNo[1].innerHTML += `<li onclick="return viewSlok(${no},${r})" class="sloks-1 p-2 mb-3 rounded-3 shadow bg-dark border-5 border-bottom border-black  ${r == 1 ? 'activeSl-1' : ''}">श्लोक - ${r}</li>`;
+        slokNo[0].innerHTML += `<li onclick="return viewSlok(${no},${r})" class="sloks-0 p-2 mb-3 rounded-3 shadow bg-dark border-5 border-bottom border-black  ${r == 1 ? 'activeSl-0' : ''}">श्लोक - ${r}</li>`;
     };
 };
 
 async function viewSlok(chNo, slNo) {
-    activeBtn('.sloks', 'activeSl');
+    activeBtn('.sloks-1', 'activeSl-1'), activeBtn('.sloks-0', 'activeSl-0');
 
     let response = await fetch(`https://vedicscriptures.github.io/slok/${chNo}/${slNo}/`);
     let rec = await response.json();
 
     slok.innerHTML = `<div class="fs-5 fw-medium d-flex flex-column row-gap-4">
-                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">${rec._id.replace('BG', 'BG - ')}</h2>
+                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">|| ${rec._id.replace('BG', '')} ||</h2>
                         <p class="fs-2 text-black" style="text-shadow: 0px 0px 15px #ff6f08">${rec.slok.replaceAll('\n', '<br>')}</p>
-                        <p>${rec.transliteration.replaceAll('\n', '<br>')}</p>
                         <hr style="border: 1px solid #000;">
-                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">Translation</h2>
-                        <p class="text-start mb-5">${rec.siva.et}</p>
-                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">Commentary</h2>
-                        <p class="text-start">${rec.siva.ec}</p>
+                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">अनुवाद</h2>
+                        <p class="text-start mb-5">${rec.rams.ht}</p>
+                        <h2 class="fw-bold" style="text-shadow:0px 2px 2px #fff">टीका</h2>
+                        <p class="text-start">${rec.rams.hc}</p>
                     </div>`;
 };
 chapters(), viewSlNo(1, 47), viewSlok(1, 1);
